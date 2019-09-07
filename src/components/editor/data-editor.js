0fae99f24916a0c93c0f68041ecf1d50805a50f6
allagan-node <allagan.node@gmail.com>
2019-09-07 14:57:23 -0700

    update

--- src/components/editor/data-editor.js
+++ src/components/editor/data-editor.js
@@ -1,7 +1,13 @@
+import { Col, Row } from "antd";
 import React from "react";
 
 const DataEditor = props => {
-  return <div>Hello, World!</div>;
+  return (
+    <Row gutter={16}>
+      <Col span={6}></Col>
+      <Col span={18}></Col>
+    </Row>
+  );
 };
 
 export default DataEditor;
