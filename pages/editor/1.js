bf09d8947fc23fd309e6312d1820317374b16058
allagan-node <allagan.node@gmail.com>
2019-09-15 22:41:58 -0700

    refactoring editor

--- pages/editor/1.js
+++ pages/editor/1.js
@@ -1,9 +1,18 @@
-import { Alert, Button, Card, Icon, Progress, Result, Steps, Upload } from "antd";
-import Router from 'next/router';
+import {
+  Alert,
+  Button,
+  Card,
+  Icon,
+  Progress,
+  Result,
+  Steps,
+  Upload
+} from "antd";
+import Router from "next/router";
 import React from "react";
 
 import EditorContext from "../../src/contexts/editor-context";
-import { ReadBytesFromFile } from '../../src/utility';
+import { ReadBytesFromFile } from "../../src/utility";
 
 class EditorFirst extends React.Component {
   static contextType = EditorContext;
@@ -21,29 +30,40 @@ class EditorFirst extends React.Component {
   }
 
   validateIndices(i, indexFiles, fileList, files, datToValidate) {
-    this.state.loadingProgress = Math.round(i / indexFiles.length * 100);
-    this.state.loadingText = '인덱스 검사 중...' + indexFiles[i].name;
+    this.state.loadingProgress = Math.round((i / indexFiles.length) * 100);
+    this.state.loadingText = "인덱스 검사 중..." + indexFiles[i].name;
     this.setState(this.state, () => {
       ReadBytesFromFile(indexFiles[i]).then(b => {
-        if (new TextDecoder().decode(b.slice(0, 6)) !== 'SqPack') {
-          this.state.errors.push(indexFiles[i].name + ' 파일 포맷이 잘못되었습니다.');
+        if (new TextDecoder().decode(b.slice(0, 6)) !== "SqPack") {
+          this.state.errors.push(
+            indexFiles[i].name + " 파일 포맷이 잘못되었습니다."
+          );
         } else {
           const dv = new DataView(b);
           const headerOffset = dv.getInt32(0xc, true);
           const numDat = dv.getUint8(headerOffset + 0x50, true);
-          
-          const fileName = indexFiles[i].name.substring(0, indexFiles[i].name.indexOf('.'));
+
+          const fileName = indexFiles[i].name.substring(
+            0,
+            indexFiles[i].name.indexOf(".")
+          );
           files[fileName] = {
             index: indexFiles[i]
           };
 
           for (let j = 0; j < numDat; j++) {
             const datFile = fileList.find(f => {
-              return f.name.substring(0, f.name.indexOf('.')) === fileName && f.name.substring(f.name.lastIndexOf('.'), f.name.length) === '.dat' + j;
+              return (
+                f.name.substring(0, f.name.indexOf(".")) === fileName &&
+                f.name.substring(f.name.lastIndexOf("."), f.name.length) ===
+                  ".dat" + j
+              );
             });
 
             if (!datFile) {
-              this.state.errors.push(fileName + ': .dat' + j + ' 파일을 찾지 못했습니다.');
+              this.state.errors.push(
+                fileName + ": .dat" + j + " 파일을 찾지 못했습니다."
+              );
             } else {
               datToValidate.push(datFile);
             }
@@ -55,7 +75,7 @@ class EditorFirst extends React.Component {
           this.validateIndices(i, indexFiles, fileList, files, datToValidate);
         } else {
           if (datToValidate.length === 0) {
-            this.state.errors.push('검사할 데이터 파일을 찾지 못했습니다.');
+            this.state.errors.push("검사할 데이터 파일을 찾지 못했습니다.");
             this.state.loading = false;
             this.setState(this.state);
           } else {
@@ -67,15 +87,26 @@ class EditorFirst extends React.Component {
   }
 
   validateDatFiles(i, datToValidate, files) {
-    this.state.loadingProgress = Math.round(i / datToValidate.length * 100);
-    this.state.loadingText = '데이터 검사 중...' + datToValidate[i].name;
+    this.state.loadingProgress = Math.round((i / datToValidate.length) * 100);
+    this.state.loadingText = "데이터 검사 중..." + datToValidate[i].name;
     this.setState(this.state, () => {
       ReadBytesFromFile(datToValidate[i].slice(0, 6)).then(b => {
-        const fileName = datToValidate[i].name.substring(0, datToValidate[i].name.indexOf('.'));
-        const datNum = datToValidate[i].name.substring(datToValidate[i].name.length - 1, datToValidate[i].name.length);
+        const fileName = datToValidate[i].name.substring(
+          0,
+          datToValidate[i].name.indexOf(".")
+        );
+        const datNum = datToValidate[i].name.substring(
+          datToValidate[i].name.length - 1,
+          datToValidate[i].name.length
+        );
 
-        if (datNum === '0' && new TextDecoder().decode(b.slice(0, 6)) !== 'SqPack') {
-          this.state.errors.push(datToValidate[i].name + ' 파일 포맷이 잘못되었습니다.');
+        if (
+          datNum === "0" &&
+          new TextDecoder().decode(b.slice(0, 6)) !== "SqPack"
+        ) {
+          this.state.errors.push(
+            datToValidate[i].name + " 파일 포맷이 잘못되었습니다."
+          );
         } else {
           files[fileName][datNum] = datToValidate[i];
         }
@@ -105,14 +136,16 @@ class EditorFirst extends React.Component {
       pass: false,
       loading: true,
       loadingProgress: 0,
-      loadingText: ''
+      loadingText: ""
     };
     this.setState(state, () => {
       const indexFiles = fileList.filter(f => {
-        return f.name.substring(f.name.lastIndexOf('.'), f.name.length) === '.index';
+        return (
+          f.name.substring(f.name.lastIndexOf("."), f.name.length) === ".index"
+        );
       });
       if (indexFiles.length === 0) {
-        this.state.errors.push('index 파일을 찾지 못했습니다.');
+        this.state.errors.push("index 파일을 찾지 못했습니다.");
         this.state.loading = false;
         this.setState(this.state);
         return;
@@ -123,49 +156,96 @@ class EditorFirst extends React.Component {
   }
 
   render() {
-    return <Card style={{ margin: "25px" }}>
-      <Steps current={0}>
-        <Steps.Step title="편집할 자료 선택" />
-        <Steps.Step title="편집" />
-        <Steps.Step title="자료 재구축" />
-      </Steps>
-      <div style={{ marginTop: '25px' }}>
-        <Alert description="예) C:\Program Files (x86)\SquareEnix\FINAL FANTASY XIV - A Realm Reborn\game\sqpack"
-        message="편집할 파이널 판타지 14 자료들이 설치되어 있는 폴더를 선택해주세요."
-        showIcon
-        type="info" />
-      </div>
-      {this.state.errors.length > 0 && <div style={{ marginTop: '25px' }}>
-        <Alert description={<ul>{this.state.errors.map((e, i) => { return <li key={i}>{e}</li>; })}</ul>}
-        message="자료 선택 오류" showIcon type="error" />
-      </div>}
-      {this.state.pass && !this.state.loading && <div style={{ marginTop: '25px' }}>
-        <Result status="success" title="자료 선택 완료!" subTitle="다음 단계로 넘어가 자료 편집을 시작하세요!" />
-      </div>}
-      {!this.state.pass && this.state.loading && <div style={{ marginTop: '25px' }}>
-        <Result icon={<Progress type="circle" percent={this.state.loadingProgress} />} title="자료 검사 중..." subTitle={this.state.loadingText} />
-      </div>}
-      {!this.state.pass && !this.state.loading && <div style={{ marginTop: '25px' }}>
-        <Upload.Dragger accept=".index*,.dat*" beforeUpload={(file, fileList) => this.validateFileList(file, fileList)}
-        customRequest={() => {}} directory={true} showUploadList={false}>
-          <p className="ant-upload-drag-icon">
-            <Icon type="inbox" />
-          </p>
-          <p className="ant-upload-text">
-            여기를 클릭하거나 자료가 담긴 폴더를 이 곳으로 드래그하여 선택하세요.
-          </p>
-          <p className="ant-upload-hint">
-            파이널 판타지 14 클라이언트 자료는 일반적으로 /game/sqpack 하위 경로에 담겨져 있습니다. sqpack 폴더를 선택해주세요.
-          </p>
-        </Upload.Dragger>
-      </div>}
-      <div style={{ marginTop: '25px' }}>
-        <Button block disabled={!this.state.pass} loading={this.state.loading} onClick={() => Router.push('/editor/2')} type="primary">
-          다음 단계로
-          <Icon type="right" />
-        </Button>
-      </div>
-    </Card>;
+    return (
+      <Card style={{ margin: "25px" }}>
+        <Steps current={0}>
+          <Steps.Step title="편집할 자료 선택" />
+          <Steps.Step title="편집" />
+          <Steps.Step title="자료 재구축" />
+        </Steps>
+        <div style={{ marginTop: "25px" }}>
+          <Alert
+            description="예) C:\Program Files (x86)\SquareEnix\FINAL FANTASY XIV - A Realm Reborn\game\sqpack"
+            message="편집할 파이널 판타지 14 자료들이 설치되어 있는 폴더를 선택해주세요."
+            showIcon
+            type="info"
+          />
+        </div>
+        {this.state.errors.length > 0 && (
+          <div style={{ marginTop: "25px" }}>
+            <Alert
+              description={
+                <ul>
+                  {this.state.errors.map((e, i) => {
+                    return <li key={i}>{e}</li>;
+                  })}
+                </ul>
+              }
+              message="자료 선택 오류"
+              showIcon
+              type="error"
+            />
+          </div>
+        )}
+        {this.state.pass && !this.state.loading && (
+          <div style={{ marginTop: "25px" }}>
+            <Result
+              status="success"
+              title="자료 선택 완료!"
+              subTitle="다음 단계로 넘어가 자료 편집을 시작하세요!"
+            />
+          </div>
+        )}
+        {!this.state.pass && this.state.loading && (
+          <div style={{ marginTop: "25px" }}>
+            <Result
+              icon={
+                <Progress type="circle" percent={this.state.loadingProgress} />
+              }
+              title="자료 검사 중..."
+              subTitle={this.state.loadingText}
+            />
+          </div>
+        )}
+        {!this.state.pass && !this.state.loading && (
+          <div style={{ marginTop: "25px" }}>
+            <Upload.Dragger
+              accept=".index*,.dat*"
+              beforeUpload={(file, fileList) =>
+                this.validateFileList(file, fileList)
+              }
+              customRequest={() => {}}
+              directory={true}
+              showUploadList={false}
+            >
+              <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+              </p>
+              <p className="ant-upload-text">
+                여기를 클릭하거나 자료가 담긴 폴더를 이 곳으로 드래그하여
+                선택하세요.
+              </p>
+              <p className="ant-upload-hint">
+                파이널 판타지 14 클라이언트 자료는 일반적으로 /game/sqpack 하위
+                경로에 담겨져 있습니다. sqpack 폴더를 선택해주세요.
+              </p>
+            </Upload.Dragger>
+          </div>
+        )}
+        <div style={{ marginTop: "25px" }}>
+          <Button
+            block
+            disabled={!this.state.pass}
+            loading={this.state.loading}
+            onClick={() => Router.push("/editor/2")}
+            type="primary"
+          >
+            다음 단계로
+            <Icon type="right" />
+          </Button>
+        </div>
+      </Card>
+    );
   }
 }
 
--- pages/editor/2.js
+++ pages/editor/2.js
@@ -1,22 +1,142 @@
+import { Alert, Card, Icon, Result, Steps } from "antd";
 import Router from "next/router";
 import React from "react";
 
 import EditorContext from "../../src/contexts/editor-context";
+import {
+  Compute,
+  DecodeDataBlocks,
+  ReadBytesFromFile,
+  UnwrapOffset
+} from "../../src/utility";
 
 class EditorSecond extends React.Component {
   static contextType = EditorContext;
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      errors: [],
+      loading: true,
+      loadingText: "",
+      dataMap: {}
+    };
   }
 
   componentDidMount() {
     if (!this.context.files.loaded) Router.push("/editor/1");
-    console.log(this.context.files);
+    if (!this.context.files["0a0000"] || !this.context.files["0a0000"].index) {
+      this.state.errors.push(
+        "트리 생성에 필요한 문자열 자료 파일을 찾을 수 없습니다. 자료를 다시 선택해주세요."
+      );
+      this.state.loading = false;
+      this.setState(this.state);
+      return;
+    }
+
+    this.state.loadingText = "문자열 자료 매핑 중...";
+    this.setState(this.state, this.constructFileMap);
+  }
+
+  constructFileMap() {
+    ReadBytesFromFile(this.context.files["0a0000"].index)
+      .then(b => {
+        const dv = new DataView(b);
+        const headerOffset = dv.getInt32(0xc, true);
+        const fileOffset = dv.getInt32(headerOffset + 0x8, true);
+        const fileCount = dv.getInt32(headerOffset + 0xc, true) / 0x10;
+
+        for (let i = 0; i < fileCount; i++) {
+          const curFileOffset = fileOffset + i * 0x10;
+          const key = dv.getUint32(curFileOffset, true);
+          const directoryKey = dv.getUint32(curFileOffset + 0x4, true);
+          const wrappedOffset = dv.getUint32(curFileOffset + 0x8, true);
+          const unwrappedOffset = UnwrapOffset(wrappedOffset);
+
+          if (!this.state.dataMap[directoryKey]) {
+            this.state.dataMap[directoryKey] = {};
+          }
+
+          this.state.dataMap[directoryKey][key] = {
+            key: key,
+            directoryKey: directoryKey,
+            datFileNum: unwrappedOffset.datFileNum,
+            datOffset: unwrappedOffset.datOffset
+          };
+        }
+      })
+      .then(() => {
+        this.state.loadingText = "문자열 자료 ExL 디코딩 중...";
+        this.setState(this.state, this.decodeExL);
+      });
+  }
+
+  decodeExL() {
+    const rootExL = this.state.dataMap[Compute("exd")][Compute("root.exl")];
+    if (!rootExL) {
+      this.state.errors.push(
+        "문자열 자료 인덱스에서 ExL을 찾을 수 없습니다. 선택한 자료가 올바른 자료인지 확인해주세요."
+      );
+      this.state.loading = false;
+      this.setState(this.state);
+      return;
+    }
+
+    rootExL.name = "root.exl";
+    rootExL.directoryName = "exd";
+
+    ReadBytesFromFile(this.context.files["0a0000"][rootExL.datFileNum])
+      .then(b => {
+        return DecodeDataBlocks(b, rootExL.datOffset);
+      })
+      .then(data => {
+        const lines = [];
+        const rootLines = new TextDecoder().decode(data).split("\n");
+        for (let i = 0; i < rootLines.length; i++) {
+          if (!rootLines[i]) continue;
+          const rootLineCols = rootLines[i].split(",");
+          if (rootLineCols.length !== 2) continue;
+          lines.push(rootLineCols[0]);
+        }
+      });
   }
 
   render() {
-    return <div>Hello, World!</div>;
+    return (
+      <Card style={{ margin: "25px" }}>
+        <Steps current={1}>
+          <Steps.Step title="편집할 자료 선택" />
+          <Steps.Step title="편집" />
+          <Steps.Step title="자료 재구축" />
+        </Steps>
+        {this.state.errors.length > 0 && (
+          <div style={{ marginTop: "25px" }}>
+            <Alert
+              description={
+                <ul>
+                  {this.state.errors.map((e, i) => {
+                    return <li key={i}>{e}</li>;
+                  })}
+                </ul>
+              }
+              message="오류"
+              showIcon
+              type="error"
+            />
+          </div>
+        )}
+        {this.state.loading && (
+          <div style={{ marginTop: "25px" }}>
+            <Result
+              icon={<Icon type="loading" />}
+              title="트리 생성 중..."
+              subTitle={this.state.loadingText}
+            />
+          </div>
+        )}
+      </Card>
+    );
   }
 }
 
